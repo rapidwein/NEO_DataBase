30f44bd64b7cc3d0e6a909c775e11628c627e75c
Varun R Sekar <rapidwein@gmail.com>
2013-04-20 19:32:33 +0530

    display script

--- pages/config.lib.php
+++ pages/config.lib.php
@@ -1,9 +1,9 @@
 <?php
 /* DEFINITIONS to connect to the database */
 define("SERVER","localhost");
-define("USERNAME","prasan");
-define("SERVER_PASSWORD","prasan123");
+define("USERNAME","root");
+define("SERVER_PASSWORD","root");
 //name of the database.
-define("DATABASE","");
+define("DATABASE","NEO");
 include_once("connection.php");
 ?>
